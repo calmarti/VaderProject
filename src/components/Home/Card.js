7de0aa6cf588c30b0b2e5e7e9531456f5ea6a368
Ivan Martinez <ivmartinezc@gmail.com>
2022-08-18 19:49:13 +0200

    tema icono de fontawesome: arreglar

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,13 +1,15 @@
 export default function Card({title, summary, imageUrl}){
 return (
-    <div style={{backgroundColor:"#F1D6C0"}} className="text-black m-8 p-8 w-1/4 rounded-3xl card">
-    <h2 className="font-bold text-3xl pb-4 text-center">{title}</h2>
-    <p class="py-2 text-lg text-justify" >
+    <div style={{backgroundColor:"#92807E"}} className="text-black m-8 p-8 w-1/4 rounded-3xl card">
+    <h2 className="font-bold text-3xl pb-4 text-center border-b-2 mt-2 max-width-full h-auto">{title}</h2>
+    <p class="py-2 text-md text-justify" >
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
       do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
       ad minim veniam.
-      <img class="my-3 ml-14" src={`${imageUrl}`} alt={title} />
-      <button class="rounded-lg text-black px-4 py-2 ml-36 border-2 border-gray-900 hover:bg-gray-100 hover:text-white hover:bg-black duration-300 mt-3">
+      
+      <img class="my-7 ml-16 inline-block" src={`${imageUrl}`} alt={title} />
+          
+      <button class="text-xl rounded-lg text-black px-4 py-2 ml-32 border-2 border-gray-900 hover:bg-gray-100 hover:text-white hover:bg-black duration-300 mt-3">
         Comprar
       </button>
     </p>
--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,5 +1,11 @@
+//TODO: pobrar con una tipografía diferente a Karla
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBrands, faSquareTwitter  } from '@fortawesome/free-solid-svg-icons'
+
 export default function Footer(){
     return(
-        <div style={{backgroundColor: "#18181B"}}className="w-screen h-36 border-t-2 border-white"></div>
+        <div style={{backgroundColor: "#18181B"}}className="w-screen h-36 border-t-2 border-white">
+            <FontAwesomeIcon style={{backgroundColor:"white"}} icon={faSquareTwitter} />
+        </div>
     )
 }
