7e6c82add3501a0fdcf9380f15ebffff7d973dc1
Ivan Martinez <ivmartinezc@gmail.com>
2022-08-20 08:48:11 +0200

    envelop icon y copyright

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,12 +1,18 @@
 //TODO: pobrar con una tipografía diferente a Karla
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSquareTwitter , faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareTwitter, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-export default function Footer(){
-    return(
-        <div style={{backgroundColor: "#18181B"}} className="w-screen h-36 border-t-2 border-white">
-            {<FontAwesomeIcon icon={faSquareTwitter} size="2x" className="icon" />}
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div
+      style={{ backgroundColor: "#18181B" }}
+      className="w-screen h-36 border-t-2 border-white"
+    >
+      <FontAwesomeIcon icon={faTwitter} size="2x" className="icon" />
+      {<FontAwesomeIcon icon={faEnvelope} size="2x" className="icon" />}
+      <p>©2022 </p>
+
+    </div>
+  );
 }
-
