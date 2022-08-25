6d6773d20724f7723e56a3f359cf5e035a56ded6
Ivan Martinez <ivmartinezc@gmail.com>
2022-08-25 19:58:51 +0200

    Hero: normalización de estilos (2)

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,18 +1,18 @@
 //TODO: problema: ancho de la imagen mayor que el ancho de viewport
 export default function Hero() {
   return (
-    <div id="hero" className= "">
+    <div className="hero">
      
-      <p
+      <h1
         style={{ fontFamily: "Courier Prime" }}
-        className="mr-96 text-6xl typing-effect"
+        className="hero-title typing-effect"
       >
         Ibsen Martínez <br/>
-      </p>
+      </h1>
      {/*  <br/> */}
-      <p className="mr-96 text-xl fade-in">
+      <span className="mr-96 text-xl fade-in">
         Escritor de ficciones, ensayos y artículos
-      </p>
+      </span>
    
     </div>
   );
